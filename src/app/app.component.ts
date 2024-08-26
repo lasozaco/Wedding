844aebdc46fed7d6394a70980fd5d29cf078dfbf
lasozaco <128002831+lasozaco@users.noreply.github.com>
2024-08-25 22:26:42 -0500

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { CoupleComponent } from "./components/couple/couple.component";
+import { EventComponent } from "./components/event/event.component";
+import { HistoryComponent } from "./components/history/history.component";
+import { GaleryComponent } from "./components/galery/galery.component";
+import { CounterComponent } from "./components/counter/counter.component";
+import { TestimonialComponent } from "./components/testimonial/testimonial.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, CoupleComponent, EventComponent, HistoryComponent, GaleryComponent, CounterComponent, TestimonialComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/couple/couple.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-couple',
+  standalone: true,
+  imports: [],
+  templateUrl: './couple.component.html',
+  styleUrl: './couple.component.css'
+})
+export class CoupleComponent {
+
+}
